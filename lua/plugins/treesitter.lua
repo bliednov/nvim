6686465f9994bc8483de51795204b26692445610
Artem Bliednov <artem@bliednov.com>
2023-12-08 19:29:08 +0100

    feat: fix the treesitter warning

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -9,6 +9,7 @@ return {
       pcall(require('nvim-treesitter.install').update { with_sync = true })
       require('nvim-treesitter.configs').setup {
         ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'vimdoc', 'vim', 'mermaid' },
+        sync_install = false,
         auto_install = true,
         highlight = { enable = true, additional_vim_regex_highlighting = false },
         indent = { enable = true, disable = { 'python' } },
